fb3870154097fdd8348826cc6936aeea8938b669
Andrew Adams <andrewjadams3@gmail.com>
2014-05-11 23:03:20 -0500

    Complete median challenge

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,14 +12,71 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+=begin
+
+Input: array of numbers
+Output: the median of the array
+
+define median(array)
+	sort the array
+	middle = array length / 2 (find middle index)
+	if array is odd length
+		array[middle] (return the middle number)
+	else
+		( array[middle] + array [middle+1] ) / 2.0 (return average of middle nums)
+	end
+end
+
+=end
+
 
 # 2. Initial Solution
 
+=begin
 
+def median(array)
+	array.sort!
+	middle = array.length / 2
+	if array.length % 2 != 0
+		array[middle]
+	else
+		(array[middle-1] + array[middle]) / 2.0
+	end
+end
+
+=end
 
 
 # 3. Refactored Solution
 
+def median(array)
+	#sort array from smallest to largest
+	array.sort!
 
+	#find the "middle" index
+	middle = array.length / 2
+
+	#if the array has an odd number of items
+	if array.length % 2 != 0
+		#return the number at the middle index
+		array[middle]
+
+	#if the array has an even number of items
+	else
+		#take the two middle numbers and return the average
+		(array[middle-1] + array[middle]) / 2.0
+	end
+end
 
 # 4. Reflection 
+=begin
+
+Since I have attempted this challenge in several other languages before (Python
+and JavaScript), I felt fairly confident implementing a solution in Ruby. This
+was good practice running through the Rspec tests before attempting to complete
+the challenge. Rspec is very interesting, I'm looking forward to writing my own
+tests soon. I like how the error messages are very descriptive, and the tests
+are written in a way that can show which parts of your code are functional and
+which aren't.
+
+=end
