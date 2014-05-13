cd258c5cf820929ba6b4728f4003c26f56b3a749
Andrew Adams <andrewjadams3@gmail.com>
2014-05-12 22:22:24 -0500

    Complete 'add it up' challenge

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,22 +4,58 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+#TOTAL
+#Input: array of numbers
+#Output: sum of all the numbers
+=begin 
 
+define total (array)
+sum up all numbers using inject
+
+=end
+
+#SENTENCE MAKER
+#Input: array of words
+#Output: single string of words separated by spaces
+# 			(with capitalization and punctuation)
+=begin
+
+define sentence maker (array)
+join array with " "
+remove trailing space
+capitalize
+add punctuation
+
+=end
 
 # 2. Initial Solution
 
+#TOTAL
+def total_initial(array)
+	array.inject(:+)
+end
 
-
+#SENTENCE MAKER
+def sentence_maker_initial(array)
+	sentence = array.join(" ")
+	sentence = sentence.capitalize.rstrip
+	sentence + "."
+end
 
 # 3. Refactored Solution
 
+def total array; array.inject(:+) end
+
+def sentence_maker array; array.join(" ").capitalize.rstrip + "." end
 
 
 # 4. Reflection 
+# I attempted this challenge mostly to learn a bit more about built in string
+# methods (I discovered strip, rstrip, and lstrip). I also had fun researching
+# how to refactor short methods onto one line. This may not be the most
+# desirable way to approach code (the second method is certainly not very
+# readable), but it was satisfying to come up with a one-liner!
